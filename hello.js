25bee175a881e57600b7adf41e4b76313db5f9e7
aditya200683-star <aditya200683@gmail.com>
2026-07-28 14:43:40 +0530

    Initial commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+function sayHello(name){
+    console.log(`Welcome to ${name}`);
+
+}
+const sayhi=(name)=>{
+    console.log(`Welcome to ${name}`);
+
+}
+function sum(a,b){
+    return a+b;
+}
+sayHello('Aditya patel')
+sayhi("Aditya patel ")
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
