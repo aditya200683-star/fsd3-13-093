7244fab3c3d9b7836018f6b48bdff7bc78424ca8
aditya200683-star <https://github.com/aditya200683-star/fsd3-13-093.git>
2026-09-16 12:21:03 +0530

    n

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,29 +1,35 @@
-import http from "http";
+import http from 'http'
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.statusCode = 200;
-    res.end("GET Request");
-  }
-  else if (req.url === "/" && req.method === "POST") {
-    res.statusCode = 200;
-    res.end("POST Request");
-  }
 
-  else if (req.url === "/" && req.method === "PUT") {
-    res.statusCode = 200;
-    res.end("PUT Request");
-  }
-
-  else if (req.url === "/" && req.method === "DELETE") {
-    res.statusCode = 200;
-    res.end("DELETE Request");
-  }
-  
-  else {
-    res.statusCode = 404;
-    res.end("request not found");
-  }
+const server =http.createServer((req,res) => {
+      if (req.url === "/" && req.method === "GET") {
+        res.statusCode = 200;
+        res.end("Get Request");
+      } else if (req.url === "/" && req.method === "POST") {
+        // console.log("Request",req);
+        let body=''
+        req.on('data',(chunk)=>{
+          body+=chunk
+        });
+        req.on("end",()=> {
+          const product=JSON.parse(body);
+          console.log("received product:",product);
+          
+        });
+        
+        res.statusCode = 200;
+        // res.end("POST Request");
+        res.end(JSON.stringify({msg:'product added',product}));
+      } 
+      else if (req.url === "/" && req.method === "PUT") {
+        res.statusCode = 200;
+        res.end("PUT Request");
+      } else if (req.url === "/" && req.method === "DELETE") {
+        res.statusCode = 200;
+        res.end("DELETE Request");
+      } else {
+        res.statusCode = 404;
+        res.end("request not found");
+      }
 });
-
-server.listen(5000, () => console.log("prg6 is running"));
+server.listen(5000,()=>console.log("prg6 is running"));
