e41f0c9b9e2a37b9197c8e79e279d695e9ee8acc
aditya200683-star <https://github.com/aditya200683-star/fsd3-13-093.git>
2026-09-22 14:46:45 +0530

    add product

---
+++ lab3/product.js
@@ -0,0 +1,17 @@
+const products = [
+    { id: 1, name: 'marker', qty: 100, price: 15 },
+    { id: 2, name: 'pen', qty: 200, price: 10 },
+    
+];
+
+let nextId=3;
+export const getAllproducts=()=>{
+    return products;
+};
+export const addProduct =( item )=>{
+    item.id=nextId;
+    nextId++;
+    products.push(item);
+    return item ;
+         
+};
